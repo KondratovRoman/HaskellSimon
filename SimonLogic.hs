7b222010f27a653dfc1539cd04596b88e93a88d5
grgrgrrr <grgrgrrr@grgrgrrr-P55-US3L.(none)>
2014-12-18 21:48:26 +0400

    Added function for level generation

--- SimonLogic.hs
+++ SimonLogic.hs
@@ -2,17 +2,23 @@ module SimonLogic where
 
 import System.Random
 import Data.List
+import System.IO
+import Control.Monad
 
 data Color = Red | Yellow | Blue | Green
 	deriving (Show, Eq)
 
+
+
 --генерация случайной доски с четырьмя цветами, которые могут повторяться
 --список представляет собой последовательность их отображения
---n - длина списка (зависит от уровня сложности)
+--n - номер уровня и количество зажигающихся цветов на этом уровне
 
-{-generateLevel :: Int -> [Color]
-generateLevel n = -}
-
+generateLevel :: Int -> IO [Color]
+generateLevel n = forM [1..n] $ (\a -> do
+			x <- randomColor
+			return x)
+	
 randomColor :: IO Color
 randomColor = do
 	number <- randomRIO (1,4) :: IO Int
@@ -26,3 +32,4 @@ getColorOnNumber n
 	| n==2 = Yellow
 	| n==3 = Blue
 	| otherwise = Green 
+
