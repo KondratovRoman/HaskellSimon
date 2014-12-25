dc558169276dcb83de2ba78b18702f107a4c9302
Grgrgrrr <grgrgrrr29@gmail.com>
2014-12-26 00:39:44 +0300

    Last version

--- BoxedCombinator1.hs
+++ BoxedCombinator1.hs
@@ -7,17 +7,9 @@ import Data.IORef
 import Data.List
 import Control.Concurrent (threadDelay)
 
-bugtext = unlines [ "Former bug: these buttons should react when clicked"
-                  , "but the boxed one does not"
-                  , ""
-                  , "Buggy   in: MacOS X [now fixed!]"
-                  , "Working in: Linux and Windows"
-                  ]
-
-
 about :: Window a -> IO ()
 about w
-  = infoDialog w "About Simon" "Simon \n\nby sfedu\n\n Simon was written using wxHaskell"
+  = infoDialog w "About Simon" "Simon \n\n Авторы: Рындеева, Кондратов, Карпенко \n\n В рамках курса \"Функциональное Программирование\"  \n Simon was written using wxHaskell \n Институт математики, механики и компьютерных наук ЮФУ, 2014\n "
 
 endOfGame :: Window a -> IO ()
 endOfGame w
@@ -26,14 +18,21 @@ endOfGame w
 chelp :: Window a -> IO ()
 chelp w
   = infoDialog w "Simon Help"
-  (  "  Kill me,please \n\n"  )
+  (  "How to play Simon\n\n"
+  ++ " Four colored buttons light up in a specific pattern.\n"
+  ++ " After displaying the pattern, the player must repeat the pattern by \n" 
+  ++ " clicking the buttons in proper order. The pattern gets longer each time \n"
+  ++ " the player completes the pattern. If the player presses a wrong button, the game ends. \n\n"  
+  ++ " Good luck!\n"
+  )
+  
 
 main :: IO ()
 main = start $ gui
 
 --Если пользователь проиграл, то обнуляем все и запускаем игру заново
-gameOver :: Window a -> IORef State -> IORef Int -> IORef State -> TextCtrl () -> IO()
-gameOver f userList n levelList label= do
+gameOver :: Window a -> IORef State -> IORef Int -> IORef State -> TextCtrl () -> TextCtrl() -> IO()
+gameOver f userList n levelList label levelInfo = do
     endOfGame f --выводим сообщение о проигрыше
     let tempList = [] --обнуляем пользовательскую последовательность
     writeIORef userList tempList 
@@ -41,76 +40,125 @@ gameOver f userList n levelList label= do
     level <- generateLevel 1
     writeIORef levelList level
     set label [ text := stringLevel level]
+    set levelInfo [ text := "Удачи в игре! "]
     return()
 
-
-
 --проверяет верно ли пользователь нажал кнопки и генерирует новый уровень
-actionGUI :: Window a -> IORef Int -> TextCtrl () -> IORef State -> IO()
-actionGUI f ref txtTitle  constState = do
+actionGUI :: [Button ()] -> [(Button(), GameColor)] -> Window a -> IORef Int -> TextCtrl () -> IORef State -> IO()
+actionGUI buttons pairs f ref txtTitle  constState = do
     st <- readIORef ref
-    --let equal = compareStates st1 st2
     state <- generateLevel st
     writeIORef constState state
-    --let level = stringLevel state
-    showColorsListWithDelay f constState txtTitle 
+    showColorsListWithDelay buttons pairs f constState txtTitle
     writeIORef ref (st+1)
     return ()
 
 -- Функция, которая выводит в лейбл один элемент сгенерированной последовательности
-showOneColor :: IORef State -> TextCtrl ()-> IO()
-showOneColor colors label = do
+showOneColor :: IORef Bool -> Window a -> [Button ()] -> [(Button(), GameColor)] -> IORef State -> TextCtrl ()-> IO()
+showOneColor refFlag f buttons pairs colors label = do
     currState <- readIORef colors
-    if currState == [] then return() else showOneColor_ colors label
+    if currState == [] then (do enableButs buttons; return()) else showOneColor_ refFlag f buttons pairs colors label
+  
+showOneColor_ :: IORef Bool -> Window a -> [Button ()] -> [(Button(), GameColor)] -> IORef State -> TextCtrl ()-> IO()
+showOneColor_ refFlag f buttons pairs colors label = do
+  currState <- readIORef colors
+  fl <- readIORef refFlag
+  let c = head currState
+  let str = colorToString c
+  let ourButton = findButtonOnColor c pairs
+  if fl == True then setColor refFlag ourButton c label else offColor refFlag ourButton label
+  if (currState == []) then return() else (if fl == False then (writeIORef colors (tail currState)) else (writeIORef colors currState))
+  
+--функция для нахождения кнопки по цвету
+findButtonOnColor :: GameColor -> [(Button(), GameColor)] -> Button()
+findButtonOnColor color list = head $ foldl (\acc x -> if (snd x == color) then fst x : acc else acc) [] list
 
-showOneColor_ :: IORef State -> TextCtrl ()-> IO()
-showOneColor_ colors label = do
-    currState <- readIORef colors
-    let color = head currState
-    let str = colorToString color
-    set label [ text := str]
-    threadDelay $ 100000 * 5
-    set label [ text := ""]
-    if (currState == []) then return() else (writeIORef colors (tail currState))
+--Красим кнопку
+setColor :: IORef Bool -> Button() -> GameColor -> TextCtrl () -> IO()
+setColor refFlag but color label
+  | color == Yellow = do
+              set but [bgcolor  := yellow ]
+              set label [ text := "Yellow" ]
+              writeIORef refFlag False
+  | color == Blue = do 
+              set but [bgcolor := blue]
+              set label [ text := "Blue" ]
+              writeIORef refFlag False
+  | color == Red = do 
+              set but [bgcolor := red]
+              set label [ text := "Red" ]
+              writeIORef refFlag False
+  | color == Green = do 
+              set but [bgcolor := green]
+              set label [ text := "Green" ]
+              writeIORef refFlag False
+  | otherwise = do
+              set but [bgcolor := black]
+
+--Убираем цвет у кнокпи
+offColor :: IORef Bool -> Button() -> TextCtrl () -> IO()
+offColor refFlag but label = do
+              set but [bgcolor := white ]
+              set label [ text := " " ]
+              writeIORef refFlag True
 
 -- Функция, которая показывает пользователю сгенерированную последовательность поэлементно с задержкой по таймеру
-showColorsListWithDelay :: Window a -> IORef State -> TextCtrl () -> IO()
-showColorsListWithDelay f generatedList label = do
+showColorsListWithDelay :: [Button ()] -> [(Button(), GameColor)] -> Window a -> IORef State -> TextCtrl () -> IO()
+showColorsListWithDelay buttons pairs f generatedList label = do
   --копируем generated list
     stateGenList <- readIORef generatedList
     generatedListCopy <- newIORef (stateGenList)
-    t <- timer f [interval := 1000, on command := showOneColor generatedListCopy label]
-    --t <- timer f [interval := 1030, on command := showOneColor generatedListCopy label]
+    flag <- newIORef True
+    t <- timer f [interval := 800, on command := showOneColor flag f buttons pairs generatedListCopy label]
     set label [ text := ""]
-    return ()
-
+    return () 
 
 -- Обнуляет пользовательский список перед генерацией нового игрового уровня и запускает новый уровень
-nullUsersList :: Window a -> IORef State -> IORef Int -> TextCtrl () -> IORef State -> IO()
-nullUsersList f userList n textField st1 = do
+nullUsersList :: [Button()] -> [(Button(), GameColor)] -> Window a -> IORef State -> IORef Int -> TextCtrl () -> IORef State -> TextCtrl() -> IO()
+nullUsersList buttons pairs f userList n textField st1 levelInfo = do
+    nn <- readIORef n
+    set levelInfo [ text := "Вы прошли уровень " ++ show (nn-1) ++ " Ура!"]
     let tempList = []
     writeIORef userList tempList
-    actionGUI f n textField st1
+    actionGUI buttons pairs f n textField st1
     return()
 
-actionUserButtons :: TextCtrl () -> IORef Int -> Window a -> IORef State -> IORef State -> GameColor -> IO()
-actionUserButtons textField n w refUser st1 butColor = do 
+--Функция, вызываемая по нажатию на кнопки
+actionUserButtons :: [Button ()] -> [(Button(), GameColor)] -> TextCtrl () -> IORef Int -> Window a -> IORef State -> IORef State -> GameColor -> TextCtrl () -> IO()
+actionUserButtons buttons pairs textField n w refUser st1 butColor levelInfo = do 
     st <- readIORef refUser
     constState <- readIORef st1
     let modifiedUserList = st ++ [butColor]
     writeIORef refUser modifiedUserList
     let partOfGenList = getNfromList (length modifiedUserList) constState  
     let equal = compareStates modifiedUserList partOfGenList  -- Сравнивает текущий подсписок с соотв. по длине подскиском программы лексико-графически
-    if (equal == False) then (gameOver w refUser n st1 textField)  else  -- Если подпоследовательность не равна соответствующей подпоследовательности списка программы, 
+    if (equal == False) then (gameOver w refUser n st1 textField levelInfo)  else  -- Если подпоследовательность не равна соответствующей подпоследовательности списка программы, 
                                                 --то была допущена ошибка, программа завершается
-                if (length modifiedUserList == length constState) then nullUsersList w refUser n textField st1 else return()
+                if (length modifiedUserList == length constState) then (do disableButs buttons; nullUsersList buttons pairs w refUser n textField st1 levelInfo ; return()) else return()
 
+--Делает кнопки недоступными для пользователя
+disableButs :: [Button ()] -> IO ()
+disableButs buttons = do 
+  set (getIfromList buttons 0) [enabled := False]
+  set (getIfromList buttons 1) [enabled := False]
+  set (getIfromList buttons 2) [enabled := False]
+  set (getIfromList buttons 3) [enabled := False]
+  return()
+
+--Делает кнопки доступными для пользователя
+enableButs :: [Button ()] -> IO ()
+enableButs buttons = do 
+  set (getIfromList buttons 0) [enabled := True]
+  set (getIfromList buttons 1) [enabled := True]
+  set (getIfromList buttons 2) [enabled := True]
+  set (getIfromList buttons 3) [enabled := True]
+  return()
 
 gui :: IO ()
 gui =  do
   -- форма, на которой будут лежать все наши контролы
-  f <- frame [ text := "Simon" ]
-  
+  f <- frame [ text := "Simon", picture := "../some/image.ico" ]
+  --set f [ image := "/some/image.ico"]
   let n = 1
   state <- generateLevel n
   let level = stringLevel state 
@@ -125,57 +173,83 @@ gui =  do
  
   -- Task text 
   txtTitle <- entry f [text := level , enabled := False ]
-  --заглушка для таймера,показывающего, сколько осталось для закрытия задания.
-  -- t <- timer f [interval := 20, on command := nextBalls vballs p]
-  --Заглушка, для таймера отсчитывающего, сколько осталось времени до конца выполнения задания
-  howManyTimer <- entry f [text := "01:29" , enabled := False ]
-  b1 <- button f [ text := "yellow", bgcolor  := yellow  ]
-  b2 <- button f [ text := "green" , bgcolor  := green ]
-  b3 <- button f [ text := "blue" , bgcolor  := blue]
-  b4 <- button f [ text := "red" , bgcolor  := red ]
+  congr <- entry f [text := "Удачи в игре!" , enabled := False ]
+      
+    -- tsakButton, мигающие кнопочки
+  taskb1 <- button f [ text := " ", bgcolor  := white, clientSize := sz 80 80, enabled := False  ]
+  taskb2 <- button f [ text := " " , bgcolor  := white, clientSize := sz 80 80, enabled := False ]
+  taskb3 <- button f [ text := " " , bgcolor  := white, clientSize := sz 80 80, enabled := False ]
+  taskb4 <- button f [ text := " " , bgcolor  := white, clientSize := sz 80 80, enabled := False ]
+  
+  let listOfPairTaskButtons = (taskb1, Yellow):(taskb2, Green):(taskb3, Blue):(taskb4, Red):[]
+  
+  p <- panel f [clientSize := sz 320 70] 
+  
+  -- gameButton, кнопочки на которые непосредственно жмакает юзерь
+  b1 <- button f [ text := " ", bgcolor  := yellow, clientSize := sz 100 100  ]
+  b2 <- button f [ text := " " , bgcolor  := green, clientSize := sz 100 100 ]
+  b3 <- button f [ text := " " , bgcolor  := blue, clientSize := sz 100 100]
+  b4 <- button f [ text := " " , bgcolor  := red, clientSize := sz 100 100 ]
+
+  --список, куда мы помещаем кнопки, на каторые жмакает юзер
+  let listUserButtons = b1:b2:b3:b4:[]
+  
+  -- Кнопки с помошщью, выходом и восхвалением 
   q <- button f [ text := "quit" , on command := close f ]
   h <- button f [ text := "help" , on command := chelp f ]
   a <- button f [ text := "about", on command := about f ]
 
 
  -- t <- timer f [interval := 1000, on command := showOneColor refState txtTitle]
-  set b1 [ on command := actionUserButtons txtTitle ref f refUserList refState Yellow]
-  set b2 [ on command := actionUserButtons txtTitle ref f refUserList refState Green]
-  set b3 [ on command := actionUserButtons txtTitle ref f refUserList refState Blue]
-  set b4 [ on command := actionUserButtons txtTitle ref f refUserList refState Red]
+  set b1 [ on command := actionUserButtons listUserButtons listOfPairTaskButtons txtTitle ref f refUserList refState Yellow congr ]
+  set b2 [ on command := actionUserButtons listUserButtons listOfPairTaskButtons txtTitle ref f refUserList refState Green congr]
+  set b3 [ on command := actionUserButtons listUserButtons listOfPairTaskButtons txtTitle ref f refUserList refState Blue congr ]
+  set b4 [ on command := actionUserButtons listUserButtons listOfPairTaskButtons txtTitle ref f refUserList refState Red congr ]
  
   set f [ layout := column 0 [
-	-- fill $ widget p
+      -- fill $ widget p
         {- верхний ряд -}
         margin 1 $ row 1 [
-	-- само задание
-        hfill $ minsize (sz 150 25) $ widget txtTitle--,
+       -- само задание
+        hfill $ minsize (sz 200 25) $ widget txtTitle
+    --,minsize (sz 150 40) $ widget  howManyTimer 
         -- Таймер до закрытия
-	      --hfill $ minsize (sz 150 25) $ widget taskShowTimer   
+       --hfill $ minsize (sz 150 25) $ widget taskShowTimer   
         ],
-        {- средний ряд -}
-        hfloatCentre $  margin 1 $ row 2 [
-         -- слева: кнопки
-           row 2 [
-             widget b1, 
-	     widget b2,
-	     widget b3,
-	     widget b4
-           ],
-	    minsize (sz 150 40) $ widget  howManyTimer 
+    
+    --переход уровня
+        margin 1 $ row 1 [
+       -- само задание
+        hfill $ minsize (sz 150 25) $  widget congr
         ],
-	{- нижний ряд -}
-        hfloatCentre $  margin 1 $ row 1 [ 
-	  widget q,
-	  widget h,
-	  widget a
-        ] 
-	--  defaultButton := q
+    
+        -- Кнопки для мигания с заданием
+      --Первый ряд
+        hfloatCentre $ margin 5 $ row 5 [
+          row 5 [ widget taskb1, widget taskb2]
+    ], 
+      
+      hfloatCentre $ margin 5 $ row 5 [
+       row 5 [ widget taskb3, widget taskb4]
+      --,minsize (sz 150 40) $ widget  howManyTimer 
+        ],  
+    
+        hfloatCentre $ margin 5 $ row 5 [ 
+      row 5 [ widget p ]
+     ] ,
+  
+        -- Непосредственно сами кнопки
+        hfloatCentre $ margin 5 $ row 5 [
+          row 5 [ widget b1, widget b2]
+    ], 
+      hfloatCentre $margin 5 $ row 5 [
+       row 5 [ widget b3, widget b4]
+      --,minsize (sz 150 40) $ widget  howManyTimer 
+        ],
+  {- нижний ряд -}
+        hfloatCentre $  margin 1 $ row 1 [ widget q, widget h, widget a ] 
+  --  defaultButton := q
      ]
    ] 
 
   return() 
-  
-
-
-
